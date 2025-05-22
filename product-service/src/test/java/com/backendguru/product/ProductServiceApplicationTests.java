cbe93434b63a57e78b7dbe15417a466cc31bb087
Hakan CİVELEK <hakancivelek5461@gmail.com>
2025-05-22 16:14:56 +0300

    added MySQL support to product-service

--- product-service/src/test/java/com/backendguru/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/backendguru/product/ProductServiceApplicationTests.java
@@ -2,9 +2,18 @@ package com.backendguru.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class ProductServiceApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0");
 
 	@Test
 	void contextLoads() {
