67e53292ab907408c2ca83f3fca19a734c355a76
hakancivelek <hakancivelek5461@gmail.com>
2025-07-26 17:16:10 +0300

    Add routes for User and Order services to API Gateway

--- api-gateway/src/main/java/com/hakancivelek/api/gateway/RouteConfiguration.java
+++ api-gateway/src/main/java/com/hakancivelek/api/gateway/RouteConfiguration.java
@@ -10,11 +10,28 @@ public class RouteConfiguration {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
+
+                // Product Service Route
                 .route(r ->
                         r.path("/web/products/**")
                                 .filters(f ->
-                                        f.rewritePath("/web/?(?<segment>.*)", "/v1/$\\{segment}"))
+                                        f.rewritePath("/web/?(?<segment>.*)", "/v1/$\\{segment}")
+                                                .removeResponseHeader("Date"))
                                 .uri("http://product-service:8080")
+                )
+
+                // User Service Route
+                .route(r ->
+                        r.path("/api/users/**")
+                                .filters(f ->
+                                        f.removeResponseHeader("Date"))
+                                .uri("http://user-service:8080")
+                )
+
+                // Order Service Route
+                .route(r ->
+                        r.path("/v1/orders/**")
+                                .uri("http://order-service:8080")
                 )
                 .build();
     }
